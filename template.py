2c4a3852b51782e22a36c5904c32506c4658b807
Jack-Crowley <jcrowley.jjc@gmail.com>
2023-12-02 16:15:11 -0500

    template

---
+++ template.py
@@ -0,0 +1,32 @@
+import itertools
+import collections
+import heapq
+import math
+import functools
+
+with open("file.txt") as data: data = [i.strip() for i in data.readlines()]
+
+def part1(data=data):
+    counter = 0
+    
+    for i in data:
+        pass
+
+    return counter
+
+def part2(data=data):
+    counter = 0
+    
+    for i in data:
+        pass
+
+    return counter
+
+SampleInput = """"""
+SampleInput = [i.strip() for i in SampleInput.split("\n")]
+
+p1 = part1()
+print("Part 1: ", p1)
+
+p2 = part2()
+print("Part 2: ", p2)
